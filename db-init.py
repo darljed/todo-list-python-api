3a8237edcdfe90f10a8c6527f0116bd7c7fffcd5
djam_devteam <djam.devteam@gmail.com>
2023-04-17 19:00:47 +0800

    updated db-init.py to set proper absolute path for the schema.sql file. This resolves the issue of file not found when db-init.py is executed outside the application's directory.

--- db-init.py
+++ db-init.py
@@ -1,9 +1,10 @@
-import sqlite3
+import sqlite3, os
 from model import Connection
 
 class DBINIT:
     def __init__(self) -> None:
         self.connection = Connection().connection
+        self.schema_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'schema.sql')
         self.db_init()
 
     def db_init(self):
@@ -11,7 +12,7 @@ class DBINIT:
 
     def initialize_tables(self):
         try:
-            with open('schema.sql') as f:
+            with open(self.schema_path) as f:
                 self.connection.executescript(f.read())
             print("tables initialized")
         except sqlite3.OperationalError as e:
